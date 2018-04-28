515a830a4c7c9894b99e99e8d6f5b66fb2e4d973
Alex Dev <alex@sistersmile.com>
2018-04-28 19:35:59 +0100

    reducers: added user and message_stack

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,52 +1,102 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import date from '../modules/date'
+import * as constants from '../constants/'
 
 export const rootReducer = combineReducers({
-    test_reducer,
+	user,
+    message_stack,
     app_config,
     router: routerReducer,
-});
+})
 
-export function test_reducer(state: any, action: any) {
-	// let new_state
-	// let modal_state
-	// switch (action.type) {
-	// 	case constants.CREATE_MODAL:
-	// 		modal_state = cloneDeep(state || [])
-	// 		modal_state.push(action.payload)
-	// 		new_state = modal_state
-	// 		break;
-	// 	case constants.DELETE_MODAL:
-	// 		modal_state = cloneDeep(state || [])
-	// 		modal_state = modal_state.filter((modal: any) => modal.id !== action.id)
-	// 		new_state = modal_state
-	// 		break;
-	// 	default:
-	// 		new_state = state || []
-	// 		break;
-	// }
-	return state || {}
+export function user(state: any, action: any){
+	let new_state
+	switch (action.type) {
+		case constants.SET_USER:
+			// scoping to allow var declarations
+			{
+				// shallow copy of the object
+				let state_user = { ...state }
+				state_user.username = action.username
+
+				new_state = state_user
+				localStorage.setItem('user', JSON.stringify(state_user))
+			}
+			break
+		default:
+			// defaults to empty array
+			if(!state){
+				const default_user = localStorage.getItem('user')
+				new_state = default_user && JSON.parse(default_user) || {}
+			} else {
+				new_state = state
+			}
+			break
+	}
+	return new_state
+}
+
+export function message_stack(state: any, action: any){
+	let new_state
+	switch (action.type) {
+		case constants.SEND_MESSAGE:
+			// scoping to allow var declarations
+			{
+				// shallow copy of the array
+				let stack = [ ...state ]
+				console.log('action', action)
+				// creating message, taking off type from the action object
+				let { type, ...message } = action
+				message.local = 1
+				message.timestamp = message.timestamp || date.format('now', 'timestamp')
+
+				stack.push(message)
+				new_state = stack
+			}
+			break
+		case constants.RECEIVE_MESSAGE:
+			// scoping to allow var declarations
+			{
+				// shallow copy of the array
+				let stack = [ ...state ]
+
+				// creating message, taking off type from the action object
+				let { type, ...message } = action
+				message.local = 0
+				message.timestamp = message.timestamp || date.format('now', 'timestamp')
+
+				stack.push(message)
+				new_state = stack
+			}
+			break
+		default:
+			// defaults to empty array
+			new_state = state || []
+			break
+	}
+	return new_state
 }
 
 export function app_config(state: any, action: any) {
 	let new_state;
 	let state_clone;
 	switch (action.type) {
-		case "APP_CONFIG":
+		case constants.APP_CONFIG:
 			state_clone = Object.assign({}, state)
-			state_clone = state_clone || {};
+			state_clone = state_clone || {}
 			if (action.property) {
-				state_clone[action.property] = action.value;
+				state_clone[action.property] = action.value
 			}
 			if (action.config && Object.keys(action.config).length) {
-				state_clone = action.config;
+				state_clone = action.config
 			}
-			new_state = { ...state, ...state_clone };
-			// localStorage.setItem('app_config', JSON.stringify(new_state.config));
+			new_state = { ...state, ...state_clone }
+			// localStorage.setItem('app_config', JSON.stringify(new_state.config))
 			break;
 		default:
-			new_state = state || {};
+			new_state = state || {}
 			break;
 	}
-	return new_state;
+	return new_state
 }
