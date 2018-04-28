fef4db6c7ff98c13de9614161c98545e2b6d6087
Alex Dev <alex@sistersmile.com>
2018-04-28 20:47:37 +0100

    <Component />: Error Boundaries

--- src/_engine/components/Component.tsx
+++ src/_engine/components/Component.tsx
@@ -2,4 +2,8 @@ import * as React from 'react'
 
 export default class Component<T> extends React.Component<any, any>{
     
+    componentDidCatch(error:any, info:any) {
+        //TODO: log error
+    }
+
 }
