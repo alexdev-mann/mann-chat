1b15fb67b8da73e5d0d80f7785180056bbe0adb1
Alex Dev <alex@sistersmile.com>
2018-04-28 22:59:30 +0100

    socket events: client_connected and client_disconnected

--- src/_engine/modules/socket.tsx
+++ src/_engine/modules/socket.tsx
@@ -33,15 +33,14 @@ const socket = {
 
     get: (data: any) => {
         return new Promise((resolve, reject) => {
-            __socket.emit('subscribe', { ...data, id: socket.req_id++ }, (response: any) => {
+            __socket.emit('get', { ...data, id: socket.req_id++ }, (response: any) => {
                 response.socket_received_time = performance.now()
-                console.warn('response:', response);
-                // if(response.data && Array.isArray(response.data)){ console.log(response.data[0], response.data.length) }
+                // console.warn('response:', response);
                 store.dispatch({ type: SOCKET_RESPONSE, id: response.id, response, api: response.api, success: response.success, reason: response.reason, logic: response.logic, sql: response.sql, timing: response.timing, source: response.source });
                 if (!response.success) {
                     reject(`${data.cmd} failed`);
                 } else {
-                    console.log(`resolving ${data.cmd}`);
+                    // console.log(`resolving ${data.cmd}`);
                     resolve(response);
                 }
             });
@@ -52,12 +51,10 @@ const socket = {
             // console.warn(data, token);
             if (!data.params) { data.params = {}; }
             if (!data.params.token && token) { console.log('token', token); data.params.token = token; }
-            console.log('sending proxy_command', data);
+            // console.log('sending proxy_command', data);
             store.dispatch({ type: SOCKET_REQUEST, id: socket.req_id, method: 'proxy', params: { ...data } });
             __socket.emit('proxy', { ...data, id: socket.req_id++ }, (response: any) => {
                 store.dispatch({ type: SOCKET_RESPONSE, id: response.id, response, api: response.api, success: response.success, reason: response.reason, logic: response.logic, sql: response.sql, timing: response.timing, source: response.source });
-                // console.log('socket object data', data);
-                // console.log('socket object response', response);
                 if (!response.success) {
                     reject(`${data.cmd} failed`)
                 } else {
@@ -72,7 +69,7 @@ const socket = {
             if (!data.token && token) { data.token = token; }
             store.dispatch({ type: SOCKET_REQUEST, id: socket.req_id, method: 'post', params: { ...data } })
             __socket.emit('post', { ...data, id: socket.req_id++ }, (response: any) => {
-                console.log('post response', response)
+                // console.log('post response', response)
                 response.data = response.params
                 store.dispatch({ type: SOCKET_RESPONSE, id: response.id, response, api: response.api, success: response.success, reason: response.reason, logic: response.logic, sql: response.sql, timing: response.timing, source: response.source });
                 // store.dispatch({ type: SOCKET_UPDATE, payload: (response.data ? response.data.payload : null), req_id: response.id, id: response.id });
@@ -84,7 +81,7 @@ const socket = {
                 if (!response.success) {
                     reject(`${data.cmd} failed`)
                 } else {
-                    console.log(response)
+                    // console.log(response)
                     resolve(response)
                 }
             });
--- src/_engine/modules/socket_connector.ts
+++ src/_engine/modules/socket_connector.ts
@@ -1,8 +1,10 @@
 import * as io from 'socket.io-client'
-// import { SOCKET_RESPONSE } from '../../constants/'
+import * as constants from '../../constants/'
 import store from '../../store/'
 import * as actions from '../../actions/'
 import config from "../../config/"
+const uuidv1 = require('uuid/v1')
+import { Chat } from '../../components/Chat'
 
 const __socket = io.connect(config.SOCKET.PROTOCOL + '://' + config.SOCKET.HOST + ':' + config.SOCKET.PORT, { transports: ['websocket'], upgrade: false, secure: true })
 
@@ -10,6 +12,23 @@ __socket.on('message_from_server', (response: any) => {
     // dispatch action
     const action = actions.receive_message(response)
     store.dispatch(action)
+})
+
+__socket.on('client_connected', (response: string) => {
+    // dispatch action
+    const action = actions.receive_message({ type: constants.RECEIVE_MESSAGE, text: (response||'Anonymous') + ' joined', username: response, id: uuidv1(), from_server: true })
+    store.dispatch(action)
+    Chat.getUsersList()
+    // console.log('client connected', response)
+
+})
+
+__socket.on('client_disconnected', (response: any) => {
+    // dispatch action
+    const action = actions.receive_message({ type: constants.RECEIVE_MESSAGE, text: (response||'Anonymous') + ' left', username: response, id: uuidv1() , from_server: true})
+    store.dispatch(action)
+    Chat.getUsersList()
+    // console.log('client disconnected', response)
 })
 
 export default __socket
