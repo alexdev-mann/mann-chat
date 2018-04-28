214190db1d14882ecc109d29c9467dd5833cee14
Alex Dev <alex@sistersmile.com>
2018-04-28 23:01:57 +0100

    TS lint bugfix

--- src/_engine/components/Component.tsx
+++ src/_engine/components/Component.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default class Component<T> extends React.Component<any, any>{
     
     componentDidCatch(error:any, info:any) {
-        //TODO: log error
+        // TODO: log error
     }
 
 }
