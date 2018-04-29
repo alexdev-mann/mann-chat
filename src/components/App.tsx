4fb6e7c7219f3f7a35a2eec6828e7f0e0fa239bb
Alex Dev <alex@sistersmile.com>
2018-04-29 16:30:07 +0100

    <App />: setBackground method
    changes background each time it’s called, could be used in the future

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,8 +7,20 @@ import { Switch } from 'react-router-dom'
 import Component from '../_engine/components/Component'
 import Globals from '../_engine/components/Globals'
 
+
 class App extends Component<any> {
-    
+    t:number = 0
+
+    // set background, changes background each time it's called
+    setBackground = () => {
+        const target = document.querySelector("body")
+        target && (target.style.backgroundImage = `url(https://source.unsplash.com/random/1280x1024?t=${this.t++})`)
+    }
+
+    componentDidMount(){
+        this.setBackground()
+    }
+
     render(){
         return <>
             <Globals render={ (globals: any) =>
